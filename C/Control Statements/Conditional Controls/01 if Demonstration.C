42eecdf25d4e5664e022368301836880e760838f
Ranjiit Sir - FORK INFOSYSTEMS <ranjiitsir@gmail.com>
2026-05-09 14:38:02 +0530

    Add files via upload

---
+++ C/Control Statements/Conditional Controls/01 if Demonstration.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    float Amount = 0.0;
+
+    printf("\n Enter Your Account Balance = ");
+    scanf("%f",&Amount);
+
+    if(Amount > 1000)
+    {
+        printf("\n\n\t WELCOME to Mall !!!");
+        printf("\n Enjoy Your Shopping !!!");
+    }
+
+    printf("\n\n Thanks For Visit !!!");
+
+    getch();
+    return 0;
+}
---
+++ C/Control Statements/Conditional Controls/Batch Codes/if.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	int No = 0;
+
+	printf("\n Enter A Number : ");
+	scanf("%d",&No);
+
+	if(No > 0)
+	{
+		printf("\n Number %d is Positive#", No);
+	}
+
+	printf("\n Thanks");
+
+	getch();
+	return 0;
+}
